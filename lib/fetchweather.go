1c92fd21cce9f9a312f2a3af11a46db70cb969eb
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-03-30 18:59:06 -0300

    feat(lib): Added complete JSON2Struct for the API JSON
    Now we've got information such as Country, Latitude, Longitude, Population etc

--- lib/fetchweather.go
+++ lib/fetchweather.go
@@ -1,4 +1,4 @@
-package lib 
+package lib
 
 import (
 	"encoding/json"
@@ -39,20 +39,106 @@ type weatherReport_t struct {
 		WindspeedKmph  string `json:"windspeedKmph"`
 		WindspeedMiles string `json:"windspeedMiles"`
 	} `json:"current_condition"`
+	NearestArea []struct {
+		AreaName []struct {
+			Value string `json:"value"`
+		} `json:"areaName"`
+		Country []struct {
+			Value string `json:"value"`
+		} `json:"country"`
+		Latitude   string `json:"latitude"`
+		Longitude  string `json:"longitude"`
+		Population string `json:"population"`
+		Region     []struct {
+			Value string `json:"value"`
+		} `json:"region"`
+		WeatherURL []struct {
+			Value string `json:"value"`
+		} `json:"weatherUrl"`
+	} `json:"nearest_area"`
+	Request []struct {
+		Query string `json:"query"`
+		Type  string `json:"type"`
+	} `json:"request"`
+	Weather []struct {
+		Astronomy []struct {
+			MoonIllumination string `json:"moon_illumination"`
+			MoonPhase        string `json:"moon_phase"`
+			Moonrise         string `json:"moonrise"`
+			Moonset          string `json:"moonset"`
+			Sunrise          string `json:"sunrise"`
+			Sunset           string `json:"sunset"`
+		} `json:"astronomy"`
+		AvgtempC string `json:"avgtempC"`
+		AvgtempF string `json:"avgtempF"`
+		Date     string `json:"date"`
+		Hourly   []struct {
+			DewPointC        string `json:"DewPointC"`
+			DewPointF        string `json:"DewPointF"`
+			FeelsLikeC       string `json:"FeelsLikeC"`
+			FeelsLikeF       string `json:"FeelsLikeF"`
+			HeatIndexC       string `json:"HeatIndexC"`
+			HeatIndexF       string `json:"HeatIndexF"`
+			WindChillC       string `json:"WindChillC"`
+			WindChillF       string `json:"WindChillF"`
+			WindGustKmph     string `json:"WindGustKmph"`
+			WindGustMiles    string `json:"WindGustMiles"`
+			Chanceoffog      string `json:"chanceoffog"`
+			Chanceoffrost    string `json:"chanceoffrost"`
+			Chanceofhightemp string `json:"chanceofhightemp"`
+			Chanceofovercast string `json:"chanceofovercast"`
+			Chanceofrain     string `json:"chanceofrain"`
+			Chanceofremdry   string `json:"chanceofremdry"`
+			Chanceofsnow     string `json:"chanceofsnow"`
+			Chanceofsunshine string `json:"chanceofsunshine"`
+			Chanceofthunder  string `json:"chanceofthunder"`
+			Chanceofwindy    string `json:"chanceofwindy"`
+			Cloudcover       string `json:"cloudcover"`
+			Humidity         string `json:"humidity"`
+			PrecipInches     string `json:"precipInches"`
+			PrecipMM         string `json:"precipMM"`
+			Pressure         string `json:"pressure"`
+			PressureInches   string `json:"pressureInches"`
+			TempC            string `json:"tempC"`
+			TempF            string `json:"tempF"`
+			Time             string `json:"time"`
+			UvIndex          string `json:"uvIndex"`
+			Visibility       string `json:"visibility"`
+			VisibilityMiles  string `json:"visibilityMiles"`
+			WeatherCode      string `json:"weatherCode"`
+			WeatherDesc      []struct {
+				Value string `json:"value"`
+			} `json:"weatherDesc"`
+			WeatherIconURL []struct {
+				Value string `json:"value"`
+			} `json:"weatherIconUrl"`
+			Winddir16Point string `json:"winddir16Point"`
+			WinddirDegree  string `json:"winddirDegree"`
+			WindspeedKmph  string `json:"windspeedKmph"`
+			WindspeedMiles string `json:"windspeedMiles"`
+		} `json:"hourly"`
+		MaxtempC    string `json:"maxtempC"`
+		MaxtempF    string `json:"maxtempF"`
+		MintempC    string `json:"mintempC"`
+		MintempF    string `json:"mintempF"`
+		SunHour     string `json:"sunHour"`
+		TotalSnowCm string `json:"totalSnow_cm"`
+		UvIndex     string `json:"uvIndex"`
+	} `json:"weather"`
 }
 
-func FetchAPIData(apiAddress string) (apiData io.ReadCloser, fetchErr error){
+func FetchAPIData(apiAddress string) (apiData io.ReadCloser, fetchErr error) {
 	/* First of all, fetch the JSON that our API (for now, wttr.in) will
 	* serve us. */
 	response, fetchErr := http.Get(apiAddress)
-	
+
 	/* Assign "apiData" to response.Body */
 	apiData = response.Body
 
 	return apiData, fetchErr
 }
 
-func Data2Struct(apiData io.ReadCloser) (weatherReport weatherReport_t, err error){
+func Data2Struct(apiData io.ReadCloser) (weatherReport weatherReport_t, err error) {
 	weatherReport = weatherReport_t{}
 	err = json.NewDecoder(apiData).Decode(&weatherReport)
 	return weatherReport, err
